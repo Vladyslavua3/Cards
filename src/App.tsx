a13ea266529a8c4ff93248d5e63a16850d49aa03
Vladyslavua <vladgershman@gmail.com>
2023-05-01 19:27:12 +0200

    update: Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,21 @@ import React from 'react'
 
 import './App.css'
 
-import { Link, Route, Routes } from 'react-router-dom'
-
-import { Test } from 'common/Test'
-import { Error } from 'components/Error/Error'
-import { Login } from 'components/Login/Login'
-import { NewPass } from 'components/Password/NewPass/NewPass'
-import { ResetPass } from 'components/Password/ResetPass/ResetPass'
-import { Profile } from 'components/Profile/Profile'
-import { Registration } from 'components/Registration/Registration'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <div style={{ display: 'flex', gap: '20px', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>Testing Routes</h1>
+        <Link to={'/'}>
+          <h1>Testing Routes</h1>
+        </Link>
         <Link to={'/login'}>Login</Link>
         <Link to={'/registration'}>Registration</Link>
         <Link to={'/profile'}>Profile</Link>
-        <Link to={'/404'}>404</Link>
-        <Link to={'/resetpass'}>Reset Password</Link>
-        <Link to={'/newpass'}>New Password</Link>
-        <Link to={'/test'}>Test</Link>
+        <Link to={'/resetPassword'}>Reset Password</Link>
+        <Link to={'/newPassword'}>New Password</Link>
       </div>
-      <Routes>
-        <Route path={'/login'} element={<Login />} />
-        <Route path={'/registration'} element={<Registration />} />
-        <Route path={'/profile'} element={<Profile />} />
-        <Route path={'/404'} element={<Error />} />
-        <Route path={'/resetpass'} element={<ResetPass />} />
-        <Route path={'/newpass'} element={<NewPass />} />
-        <Route path={'/test'} element={<Test />} />
-      </Routes>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,16 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
 
 import { store } from 'bll/store'
+import { router } from 'routes/Routes'
 
 // eslint-disable-next-line import/no-named-as-default-member
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </HashRouter>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
